2d7f1b26fe33aedf6ade456593f7b49591318da4
3846masa <3846masahiro+git@gmail.com>
2018-07-14 18:44:37 +0900

    Rewrite tests with macro

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,4 @@
-import test from 'ava';
+import test, { ExecutionContext } from 'ava';
 import UnicodeRange from '../src';
 
 function closedInterval(start: number, end: number) {
@@ -9,110 +9,50 @@ function closedInterval(start: number, end: number) {
   return arr;
 }
 
-// UnicodeRange.parse
-test('U+20', t => {
-  const unicodeRangeArray = ['U+20'];
-  const expected = [0x20];
-  const actual = UnicodeRange.parse(unicodeRangeArray);
+function equalsMacro(t: ExecutionContext<{}>, input: any, expected: any) {
+  const actual = UnicodeRange.parse(input);
   t.deepEqual(actual, expected);
+}
+Object.assign(equalsMacro, {
+  title: (_: string, input: string[]) => input.join(',\x20'),
 });
 
-test('U+30-39', t => {
-  const unicodeRangeArray = ['U+30-39'];
-  const expected = closedInterval(0x30, 0x39);
-  const actual = UnicodeRange.parse(unicodeRangeArray);
-  t.deepEqual(actual, expected);
-});
+const equalsTestCaseList = [
+  {
+    values: ['U+20'],
+    expected: [0x20],
+  },
+  {
+    values: ['U+30-39'],
+    expected: closedInterval(0x30, 0x39),
+  },
+  {
+    values: ['U+3?'],
+    expected: closedInterval(0x30, 0x3f),
+  },
+  {
+    values: ['U+??'],
+    expected: closedInterval(0x0, 0xff),
+  },
+];
 
-test('U+3?', t => {
-  const unicodeRangeArray = ['U+3?'];
-  const expected = closedInterval(0x30, 0x3f);
-  const actual = UnicodeRange.parse(unicodeRangeArray);
-  t.deepEqual(actual, expected);
-});
+equalsTestCaseList.forEach(testCase => test(equalsMacro, testCase.values, testCase.expected));
 
-test('U+??', t => {
-  const unicodeRangeArray = ['U+??'];
-  const expected = closedInterval(0x0, 0xff);
-  const actual = UnicodeRange.parse(unicodeRangeArray);
-  t.deepEqual(actual, expected);
-});
-
-// Errors
-test('U+XX', t => {
-  const unicodeRangeArray = ['U+XX'];
+function invalidMacro(t: ExecutionContext<{}>, input: any) {
   t.throws(
     () => {
-      UnicodeRange.parse(unicodeRangeArray);
+      UnicodeRange.parse([input]);
     },
     {
       instanceOf: TypeError,
-      message: '"U+XX" is invalid unicode-range.',
+      message: `"${input}" is invalid unicode-range.`,
     },
   );
+}
+Object.assign(invalidMacro, {
+  title: (_: string, input: string[]) => `Invalid range test: ${input}`,
 });
 
-test('U+1?3', t => {
-  const unicodeRangeArray = ['U+1?3'];
-  t.throws(
-    () => {
-      UnicodeRange.parse(unicodeRangeArray);
-    },
-    {
-      instanceOf: TypeError,
-      message: '"U+1?3" is invalid unicode-range.',
-    },
-  );
-});
+const invalidTestCaseList = ['U+XX', 'U+1?3', 'U+1234567', 'U+123456-1234567', 'U+3?-40', 'U+??????'];
 
-test('U+1234567', t => {
-  const unicodeRangeArray = ['U+1234567'];
-  t.throws(
-    () => {
-      UnicodeRange.parse(unicodeRangeArray);
-    },
-    {
-      instanceOf: TypeError,
-      message: '"U+1234567" is invalid unicode-range.',
-    },
-  );
-});
-
-test('U+123456-1234567', t => {
-  const unicodeRangeArray = ['U+123456-1234567'];
-  t.throws(
-    () => {
-      UnicodeRange.parse(unicodeRangeArray);
-    },
-    {
-      instanceOf: TypeError,
-      message: '"U+123456-1234567" is invalid unicode-range.',
-    },
-  );
-});
-
-test('U+3?-40', t => {
-  const unicodeRangeArray = ['U+3?-40'];
-  t.throws(
-    () => {
-      UnicodeRange.parse(unicodeRangeArray);
-    },
-    {
-      instanceOf: TypeError,
-      message: '"U+3?-40" is invalid unicode-range.',
-    },
-  );
-});
-
-test('U+??????', t => {
-  const unicodeRangeArray = ['U+??????'];
-  t.throws(
-    () => {
-      UnicodeRange.parse(unicodeRangeArray);
-    },
-    {
-      instanceOf: TypeError,
-      message: '"U+??????" is invalid unicode-range.',
-    },
-  );
-});
+invalidTestCaseList.forEach(testCase => test(invalidMacro, testCase));
